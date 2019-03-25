9fe51623f166cb976970c7b8cac6f3e3fcf5272b
Byron Sakiadis <byronl@udel.edu>
2019-03-24 21:43:37 -0400

    Added basic argument parsing, but need to actually use the arguments in my logic...

--- watch_dog.py
+++ watch_dog.py
@@ -2,9 +2,33 @@ import pyinotify as pyi
 import sqlite3
 import sys
 import functools
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--passwd-file', default="/etc/passwd",
+                    help='Path to passwd file, defaults to /etc/passwd')
+parser.add_argument('--group-file', default="/etc/group",
+                    help='Path to passwd file, defaults to /etc/group')
+parser.add_argument('--database-path', default="/tmp/watch_dog_db",
+                    help='Path to database file, defaults to /tmp/watch_dog_db')
+
+args = parser.parse_args()
+
+passwd_file_path = args.passwd_file
+group_file_path = args.group_file
+database_file_path = args.database_path
+
+try:
+    open(passwd_file_path, 'r')
+    open(group_file_path, 'r')
+    open(database_file_path, 'rw')
+except IOError as e:
+    print(e)
+    sys.exit(1)
+
 
 def write_passwd_changes(connection):
-    passwd_file = open('/etc/passwd', 'r')
+    passwd_file = open(passwd_file_path, 'r')
     insert = "INSERT INTO USER_PASSWD (USERNAME,UID,GID,INFO, HOME_DIR,SHELL) VALUES (?,?,?,?,?,?);"
     rows = []
     for i in passwd_file:
@@ -13,7 +37,7 @@ def write_passwd_changes(connection):
         rows.append(row)
     connection.executemany(insert,rows)
     connection.commit()
-    out = connection.execute("SELECT * FROM USER_PASSWD")
+    #out = connection.execute("SELECT * FROM USER_PASSWD")
 
 #example mostly taken from here: https://github.com/seb-m/pyinotify/blob/master/python2/examples/loop.py
 
@@ -28,23 +52,10 @@ notifier = pyi.Notifier(wm)
 
 
 conn = sqlite3.connect('test.db')
-
-print "Opened database successfully";
+##print "Opened database successfully";
 
 write_func = functools.partial(write_passwd_changes, conn)
 
-# Notifier instance spawns a new process when daemonize is set to True. This
-# child process' PID is written to /tmp/pyinotify.pid (it also automatically
-# deletes it when it exits normally). Note that this tmp location is just for
-# the sake of the example to avoid requiring administrative rights in order
-# to run this example. But by default if no explicit pid_file parameter is
-# provided it will default to its more traditional location under /var/run/.
-# Note that in both cases the caller must ensure the pid file doesn't exist
-# before this method is called otherwise it will raise an exception.
-# /tmp/pyinotify.log is used as log file to dump received events. Likewise
-# in your real code choose a more appropriate location for instance under
-# /var/log (this file may contain sensitive data). Finally, callback is the
-# above function and will be called after each event loop.
 try:
     notifier.loop(daemonize=True, callback=write_func,
                   pid_file='/tmp/pyinotify.pid', stdout='/tmp/pyinotify.log')

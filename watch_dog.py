679080b5c136613acd4d64036efacd8cdc774121
Byron Sakiadis <byronl@udel.edu>
2019-03-30 16:27:16 -0400

    For some reason, this method isn't working - going to fall back to polling at a regular time interval

--- watch_dog.py
+++ watch_dog.py
@@ -4,6 +4,7 @@ import sys
 import functools
 import argparse
 import os
+from time import sleep
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--passwd-file', default="/etc/passwd",
@@ -12,7 +13,7 @@ parser.add_argument('--group-file', default="/etc/group",
                     help='Path to passwd file, defaults to /etc/group')
 parser.add_argument('--database-path', default="/tmp/watch_dog_db.sqlite",
                     help='Path to database file, defaults to /tmp/watch_dog_db.sqlite')
-parser.add_argument('--log-file-path', default="/tmp/watch_dog_log",
+parser.add_argument('--log-file-path', default="/tmp/watch_dog.log",
                     help='Path to database file, defaults to /tmp/watch_dog.log')
 
 args = parser.parse_args()
@@ -33,14 +34,14 @@ except IOError as e:
     sys.exit(1)
 
 class GroupPasswdEvent(pyi.ProcessEvent):
-    def my_init(self, log_file, connection,passwd_path,group_path):
+    def my_init(self, log_file, db_path,passwd_path,group_path):
         """
         This is your constructor it is automatically called from
         ProcessEvent.__init__(), And extra arguments passed to __init__() would
         be delegated automatically to my_init().
         """
         self.log_file = log_file
-        self.sqlite_connection = connection
+        self.db_path = db_path
         self.passwd_path = passwd_path
         self.group_path = group_path
 
@@ -49,16 +50,18 @@ class GroupPasswdEvent(pyi.ProcessEvent):
         Eventually, this method is called for all others types of events.
         This method can be useful when an action fits all events.
         """
-        self.log_file.write(str(event) + '\n')
-        self.log_file.write(str(event.path) + '\n')
-        self.log_file.flush()
+        print(str(event))
         if event.path == passwd_file_path:
             self.write_passwd_changes()
+            print("would write pwd file here")
         if event.path == group_file_path:
             self.write_group_changes()
+            print("would write group file here")
+        print("in close_write event")
 
     def write_group_changes(self):
-        group_file = open(group_file_path, 'r')
+        group_file = open("/etc/group", 'r')
+        conn = sqlite3.connect(self.db_path)
         delete = "DELETE FROM USER_GROUP;"
         insert = "INSERT OR REPLACE INTO USER_GROUP (GROUP_NAME, GID, GROUP_LIST) VALUES (?,?,?)"
         rows = []
@@ -66,12 +69,16 @@ class GroupPasswdEvent(pyi.ProcessEvent):
             row = i.strip("\n").split(":")
             del(row[1])
             rows.append(row)
-        self.sqlite_connection.execute(delete)
-        self.sqlite_connection.executemany(insert,rows)
-        self.sqlite_connection.commit()
+        conn.execute(delete)
+        conn.executemany(insert,rows)
+        conn.commit()
+        conn.close()
+        group_file.close()
+        print("in group_changes")
 
     def write_passwd_changes(self):
-        passwd_file = open(passwd_file_path, 'r')
+        passwd_file = open("/etc/passwd", 'r')
+        conn = sqlite3.connect(self.db_path)
         delete = "DELETE FROM USER_PASSWD;"
         insert = "INSERT OR REPLACE INTO USER_PASSWD (USERNAME,UID,GID,INFO, HOME_DIR,SHELL) VALUES (?,?,?,?,?,?);"
         rows = []
@@ -79,9 +86,15 @@ class GroupPasswdEvent(pyi.ProcessEvent):
             row = i.strip("\n").split(":")
             del(row[1])
             rows.append(row)
-        self.sqlite_connection.execute(delete)
-        self.sqlite_connection.executemany(insert,rows)
-        self.sqlite_connection.commit()
+        conn.execute(delete)
+        conn.executemany(insert,rows)
+        conn.commit()
+        conn.close()
+        passwd_file.close()
+        print("in write_passwd_changes")
+
+    def clean_up(self):
+        self.log_file.close()
 
 def table_create(connection):
     #iterate through files in ./sql directory
@@ -93,34 +106,33 @@ def table_create(connection):
             try:
                 cursor.executescript(our_sql)
             except Exception as e:
+                print(e)
                 cursor.close()
         else:
             continue
-    #run each as a query
 
-def initial_load(connection,groupPasswdEvent):
-    groupPasswdEvent.write_group_changes()
-    groupPasswdEvent.write_passwd_changes()
+def initial_load(connection,GroupPasswdEventhandler):
+    GroupPasswdEventhandler.write_group_changes(None)
+    GroupPasswdEventhandler.write_passwd_changes(None)
 
 watches = []
 watches.append(passwd_file_path)
 watches.append(group_file_path)
 #we only really care if this file is changed, not if it's opened
 
-log = open('/tmp/watch_dog.log','w')
+log = open(log_file_path,'w')
 
 conn = sqlite3.connect(database_file_path)
 table_create(conn)
 conn.commit()
-
+conn.close()
+pyi.log.setLevel(10)
 try:
     wm = pyi.WatchManager()
-    # It is important to pass named extra arguments like 'fileobj'.
-    event_handler = GroupPasswdEvent(log_file=log,connection=conn,passwd_path=passwd_file_path,group_path=group_file_path)
-    initial_load(conn,event_handler)
-    notifier = pyi.Notifier(wm, default_proc_fun=event_handler)
-    wm.add_watch(watches, pyi.IN_MODIFY)
+    event_handler = GroupPasswdEvent(log_file=log,db_path=database_file_path,passwd_path=passwd_file_path,group_path=group_file_path)
+    wm.add_watch(watches, pyi.IN_CLOSE_WRITE)
+    notifier = pyi.Notifier(wm,event_handler,read_freq=5,threshold=64,timeout=500)
     notifier.loop()
 finally:
-    conn.close()
-    log.close()
+    print("Closing files")
+    event_handler.clean_up()

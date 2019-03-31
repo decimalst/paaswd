a36db627f5a18eed0ec112e3b6814f9f73293608
Byron Sakiadis <byronl@udel.edu>
2019-03-31 18:03:33 -0400

    Added the queriable endpoints to the API

--- paaswd-app/paaswd/api.py
+++ paaswd-app/paaswd/api.py
@@ -9,10 +9,6 @@ from paaswd.db import get_db
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-@bp.route('/register', methods=['GET'])
-def register():
-    return "register"
-
 @bp.route('/users', methods=['GET'])
 def all_users():
     db = get_db()
@@ -21,7 +17,41 @@ def all_users():
 
 @bp.route('/users/query', methods=['GET'])
 def users_query():
-    return "register"
+    db = get_db()
+    if "name" in request.args.keys():
+        name = request.args.get("name")
+    else:
+        name = "%"
+
+    if "uid" in request.args.keys():
+        uid = request.args.get("uid")
+    else:
+        uid = "%"
+
+    if "gid" in request.args.keys():
+        gid = request.args.get("gid")
+    else:
+        gid = "%"
+
+    if "comment" in request.args.keys():
+        comment = request.args.get("comment")
+    else:
+        comment = "%"
+
+    if "home" in request.args.keys():
+        home = request.args.get("home")
+    else:
+        home = "%"
+
+    if "shell" in request.args.keys():
+        shell = request.args.get("shell")
+    else:
+        shell = "%"
+    args = [name, uid, gid, comment, home, shell]
+    print(args)
+    query = "SELECT * FROM USER_PASSWD WHERE USERNAME LIKE ? AND UID LIKE ? \
+            AND GID LIKE ? AND INFO LIKE ? AND HOME_DIR LIKE ? AND SHELL LIKE ?;"
+    return json.dumps([tuple(row) for row in db.execute(query,args).fetchall()])        
 
 @bp.route('/users/<int:user_id>', methods=['GET'])
 def user_from_uid(user_id):
@@ -29,7 +59,7 @@ def user_from_uid(user_id):
     query = "SELECT * FROM USER_PASSWD WHERE UID=?;"
     return json.dumps([tuple(row) for row in db.execute(query,[str(user_id)]).fetchall()])
 
-@bp.route('/users/<user_id>/groups', methods=['GET'])
+@bp.route('/users/<int:user_id>/groups', methods=['GET'])
 def user_group_membership(user_id):
     db = get_db()
     subquery = "SELECT USERNAME FROM USER_PASSWD WHERE UID = ?;"
@@ -45,7 +75,45 @@ def all_groups():
 
 @bp.route('/groups/query', methods=['GET'])
 def groups_query():
-    return "register"
+    db = get_db()
+    if "name" in request.args.keys():
+        name = request.args.get("name")
+    else:
+        name = "%"
+
+    if "gid" in request.args.keys():
+        gid = request.args.get("gid")
+    else:
+        gid = "%"
+    #Okay, so this logic sucks, but I didn't have a way to easily get the intersection of the
+    #groups list passed in and the groups list in the column. It'd basically be if the 
+    #intersection of the list and intersection of the column is a size greater than zero,
+    #then return the row
+    add_string = []
+    if "member" in request.args.keys():
+        for _ in request.args.getlist("member"):
+            add_string.append("GROUP_LIST LIKE ?")
+    else:
+        member = "%"
+    query_groups = "SELECT * FROM USER_GROUP WHERE GROUP_NAME LIKE ? AND GID LIKE ? AND GROUP_LIST LIKE ?;"
+    if(len(add_string)>1):
+        query_groups = "SELECT * FROM USER_GROUP WHERE GROUP_NAME LIKE ? AND GID LIKE ? AND (" 
+        for i in range(len(add_string)-1):
+            query_groups += add_string[1] + " OR "
+        query_groups += add_string[len(add_string)-1] + ");"
+        print(query_groups)
+        members = []
+        for i in request.args.getlist("member"):
+            members.append("%" + i + "%")
+        args = [name,gid,*members]
+    elif(len(add_string)==1):
+        member = "%" + request.args.get("member") + "%"
+        args = [name,gid,member]
+    else:
+        args = [name,gid,member]
+    print(args)
+    return json.dumps([tuple(row) for row in db.execute(query_groups,args).fetchall()])
+    return "test"
 
 @bp.route('/groups/<int:group_id>', methods=['GET'])
 def groups_from_gid(group_id):
